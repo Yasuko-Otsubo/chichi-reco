6782883b79f3037c7e0e88d05f0e985fbb117e59
yasuko <y.otsubo04@gmail.com>
2025-11-02 18:25:22 +0900

    get項目作成

--- src/app/api/profiles/route.ts
+++ src/app/api/profiles/route.ts
@@ -6,7 +6,6 @@ const prisma = new PrismaClient()
 export const POST = async ( request: Request/*, context: any*/ ) => {
   try  {
     const body = await request.json()
-
     const { id, name, supabase_user_id, height, target_weight} = body
 
     const data = await prisma.profiles.create({
@@ -28,4 +27,15 @@ export const POST = async ( request: Request/*, context: any*/ ) => {
       return NextResponse.json({ status: error.message}, { status: 400 })
     }  
   }
-}
+};
+
+  export const GET = async (request: NextRequest) => {
+    try {
+      const profiles = await prisma.profiles.findMany()
+      return NextResponse.json({ status: 'OK', profiles}, { status: 200})
+    } catch (error) {
+      if(error instanceof Error) {
+        return NextResponse.json({status: error.message}, { status: 400})
+      }
+    }
+  }
