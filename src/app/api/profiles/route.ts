6c8b561d5176fdd3b6360deb07ac488be2830835
yasuko <y.otsubo04@gmail.com>
2025-11-03 21:40:52 +0900

    get処理の追加

--- src/app/api/profiles/route.ts
+++ src/app/api/profiles/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 
 const prisma = new PrismaClient()
 
@@ -38,4 +40,43 @@ export const POST = async ( request: Request/*, context: any*/ ) => {
         return NextResponse.json({status: error.message}, { status: 400})
       }
     }
-  }
+  };
+
+
+  export const PUT = async (
+    request: NextRequest, 
+    { params } : { params : { id: string }},
+  ) => {
+    const { id } = params;    
+    const { name, email, password, height, target_weight } = await request.json();
+
+    try {
+      const supabase = createServerComponentClient({ cookies });
+
+      if (email) {
+        const { error: emailError } = await supabase.auth.updateUser({ email });
+        if(emailError) throw new Error(`メール更新失敗: ${emailError.message}`);
+      }
+
+      if (password) {
+        const{ error: passwordError } = await supabase.auth.updateUser({ password });
+        if(passwordError) throw new Error(`パスワード更新失敗: ${passwordError.message}`);
+      }
+
+      const profile = await prisma.profiles.update({
+        where: { 
+          id: parseInt(id),
+         },
+        data: {
+          name,
+          height,
+          target_weight,
+        },
+      })
+
+      return NextResponse.json({status: 'OK', profile}, { status: 200})
+    } catch (error) {
+      if (error instanceof Error) 
+        return NextResponse.json({ status: error.message }, { status: 400})
+    }
+  } 
