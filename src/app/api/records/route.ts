570b7c427d637cd2e81a8530b36c82310a66c98d
yasuko <y.otsubo04@gmail.com>
2025-11-10 01:27:31 +0900

    post作成中（エラー修正模索）

---
+++ src/app/api/records/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const POST = async (request: Request/*, context: any*/) => {
+  try {
+    const body = await request.json();
+    const { /*user_id, */date, weight, steps, memo } = body;
+
+    const data = await prisma.records.create({
+      data: {
+        /*user_id,*/
+        date,
+        weight,
+        steps,
+        memo,
+      },
+    });
+    return NextResponse.json({
+      status: "OK",
+      message: "記録しました",
+      id: data.id,
+    });
+  } catch (error) {
+    if ( error instanceof Error) {
+      return NextResponse.json({ status: error.message}, { status: 400});
+    }
+  }
+} ;
