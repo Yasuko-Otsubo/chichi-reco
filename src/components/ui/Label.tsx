f181bf1e86dca25e4013b353e556f10b386bf931
yasuko <y.otsubo04@gmail.com>
2025-12-21 22:44:39 +0900

    Labelのコンポーネント化

---
+++ src/components/ui/Label.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/utils/utils";
+
+interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement>{
+  required?: boolean;
+}
+
+export const Label = ({
+  children,
+  required,
+  className,
+  ...props
+}: LabelProps) => {
+  return (
+    <label
+    className={cn("block text-sm font-medium text-gray-900", className)}
+    {...props}
+    >
+      {children}
+      {required  && <span  className="ml-1 text-ret-500 ">*</span>}
+    </label>
+  )
+}
