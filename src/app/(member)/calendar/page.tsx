640616373b443277927eef9057c25d1f2ebd4440
yasuko <y.otsubo04@gmail.com>
2026-01-12 18:05:02 +0900

    supabaseからその月の記録を取得する

--- src/app/(member)/calendar/page.tsx
+++ src/app/(member)/calendar/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { supabase } from "@/utils/supabase";
+
 
 type Record = {
   date: string;
@@ -17,15 +19,34 @@ type CalendarCell = {
 
 
 export default function Page() {
+  //月の開始日と終了日を作る関数
   const getMonthRange = (year: number, month: number) => {
-    const start = new Date(year, month, 1); //月の開始日
-    const end = new Date(year, month + 1, 0 );//月の終了日
+    const start = new Date(year, month, 1); 
+    const end = new Date(year, month + 1, 0 );
 
     return {
       startStr: start.toISOString().slice(0, 10),
       endStr: end.toISOString().slice(0, 10),
+    };
+  };
+    
+  //その月の記録を取得する関数
+  const fetchMonthlyRecords = async (year: number, month: number) => {
+    const { startStr, endStr } = getMonthRange(year, month);
+
+    const { data, error } = await supabase
+    .from("records")
+    .select("*")
+    .gte("date", startStr)
+    .lte("date", endStr);
+
+    if(error) {
+      console.log("Supabase error", error);
+      return[];
     }
-  }
+    return data as Record[]
+};
+
 
 return(
   <>
