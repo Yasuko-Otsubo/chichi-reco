28eb467b091618ef360fd246de547b32e0341f47
yasuko <y.otsubo04@gmail.com>
2026-01-12 17:47:17 +0900

    start, endの作成

--- src/app/(member)/calendar/page.tsx
+++ src/app/(member)/calendar/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+
 type Record = {
   date: string;
   weight: number;
@@ -16,6 +17,15 @@ type CalendarCell = {
 
 
 export default function Page() {
+  const getMonthRange = (year: number, month: number) => {
+    const start = new Date(year, month, 1); //月の開始日
+    const end = new Date(year, month + 1, 0 );//月の終了日
+
+    return {
+      startStr: start.toISOString().slice(0, 10),
+      endStr: end.toISOString().slice(0, 10),
+    }
+  }
 
 return(
   <>
