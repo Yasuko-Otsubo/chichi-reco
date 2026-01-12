dbd39c0a834bf2c790a8e7247ea2ac7428e1d899
yasuko <y.otsubo04@gmail.com>
2026-01-12 16:57:39 +0900

    calendarの型定義

--- src/app/(member)/calendar/page.tsx
+++ src/app/(member)/calendar/page.tsx
@@ -1,5 +1,20 @@
 'use client'
 
+type Record = {
+  date: string;
+  weight: number;
+  steps: number;
+  memo: string;
+}
+
+type CalendarCell = {
+  date: string;
+  day: number;
+  weight?: number;
+  delta?: number;
+}
+
+
 export default function Page() {
 
 return(
