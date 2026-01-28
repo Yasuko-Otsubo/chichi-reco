1d7f7c671f7977376c19f37d5607ddffc463f5a8
yasuko <y.otsubo04@gmail.com>
2026-01-28 23:52:27 +0900

    認証

---
+++ src/app/api/records/[range]/route.ts
@@ -0,0 +1,21 @@
+import { prisma } from "@/app/_libs/prisma";
+import { getAuthenticatedUser } from "@/app/_libs/supabase/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (request: NextRequest) => {
+  try {
+    const user = await getAuthenticatedUser(request);
+
+    const profile = await prisma.profile.findFirst({
+      where: { supabaseUserId: user.id },
+    });
+
+    if(!profile) {
+      return NextResponse.json(
+        { status: "NG", message: "Profile not found" },
+        { status: 404 }
+      );
+    }
+
+  }
+}
