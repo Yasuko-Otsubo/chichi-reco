7d9da9dc1824b15dcc562eaa44f4555a4c22de9b
yasuko <y.otsubo04@gmail.com>
2026-02-01 22:30:17 +0900

    型修正

--- src/types/profiles.ts
+++ src/types/profiles.ts
@@ -1,22 +1,36 @@
+import { Profile as PrismaProfile } from "@/app/generated/prisma/client";
+//レスポンス用
 export type ProfileFields = {
   id: number;
-  name?: string;
+  name: string | null;
   supabaseUserId: string;
-  height?: number;
-  targetWeight?: number;
+  height: number | null;
+  targetWeight: number | null;
 };
+
+//POST用
+export type ProfileCreateRequest = {
+  name: string | null;
+  height: number | null;
+  targetWeight: number | null;
+};
+
+//DB ⇒ APIの変換
+export const toProfileFields = (profile: PrismaProfile): ProfileFields => ({
+  id: profile.id,
+  name: profile.name,
+  supabaseUserId: profile.supabaseUserId,
+  height: profile.height,
+  targetWeight: profile.targetWeight,
+
+})
 
 export type ProfileResponse = {
   status: "OK" | "NG";
   message: string;
   profiles?: ProfileFields[];
 };
-//新規作成用
-export type ProfileCreateRequest = {
-  name: string;
-  height: number;
-  targetWeight: number;
-};
+
 
 //更新用
 export type ProfileUpdateRequest = {
