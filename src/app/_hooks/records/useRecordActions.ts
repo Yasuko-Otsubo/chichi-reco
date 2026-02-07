4fa77a629c74c68271665d0850c1c0bfb3d4deb3
yasuko <y.otsubo04@gmail.com>
2026-02-07 22:16:27 +0900

    useRecordCreate を追加

---
+++ src/app/_hooks/records/useRecordActions.ts
@@ -0,0 +1,24 @@
+import { useSupabaseSession } from "../useSupabaseSession"
+import { TodayFormData } from "@/app/(member)/today/[date]/TodayForm";
+
+export const useRecordCreate = () => {
+  const { token } = useSupabaseSession();
+
+  const createRecord = async (data: TodayFormData) => {
+    const res = await fetch("/api/records", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        date: data.date,
+        weight: data.weight !== "" ? Number(data.weight): null,
+        steps: data.steps !== "" ? Number(data.steps): null,
+        memo: data.memo !== "" ? data.memo: null,
+      }),
+    });
+  return res;
+  };
+  return { createRecord };
+}
