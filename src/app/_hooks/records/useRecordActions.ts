341eb51d5472dc613532fe722d6d0e5b9ca7c657
yasuko <y.otsubo04@gmail.com>
2026-02-08 00:47:59 +0900

    headers, bodyの共通化

--- src/app/_hooks/records/useRecordActions.ts
+++ src/app/_hooks/records/useRecordActions.ts
@@ -4,38 +4,37 @@ import { TodayFormData } from "@/app/(member)/today/[date]/TodayForm";
 export const useRecordCreate = () => {
   const { token } = useSupabaseSession();
 
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  }
+
+  const buildRequestBody = (data: TodayFormData) => ({
+    date: data.date,
+    weight: data.weight !== "" ? Number(data.weight): null,
+    steps: data.steps !== "" ? Number(data.steps): null,
+    memo: data.memo !== "" ? data.memo: null,
+  });
+
+  //create
   const createRecord = async (data: TodayFormData) => {
     const res = await fetch("/api/records", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        date: data.date,
-        weight: data.weight !== "" ? Number(data.weight): null,
-        steps: data.steps !== "" ? Number(data.steps): null,
-        memo: data.memo !== "" ? data.memo: null,
-      }),
+      headers,
+      body: JSON.stringify(buildRequestBody(data)),
     });
   return res;
   };
 
+  //update
   const updateRecord = async (id: string, data: TodayFormData) => {
     const res = await fetch(`/api/records/${id}`, {
       method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        date: data.date,
-        weight: data.weight !== "" ? Number(data.weight): null,
-        steps: data.steps !== "" ? Number(data.steps): null,
-        memo: data.memo !== "" ? data.memo: null,
-      }),
+      headers,
+      body: JSON.stringify(buildRequestBody(data)),
     });
     return res;
-  }
+  };
+  
   return { createRecord, updateRecord };
 }
