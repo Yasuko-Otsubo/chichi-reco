4baba3e3051c731937c748640f3f28a457ba1242
yasuko <y.otsubo04@gmail.com>
2026-02-11 21:45:03 +0900

    前回の記録表示、新規or編集の判定追加

--- src/app/(member)/today/[date]/page.tsx
+++ src/app/(member)/today/[date]/page.tsx
@@ -1,34 +1,70 @@
 import { supabase } from "@/app/_libs/supabase";
-import TodayForm from "./TodayForm";
+import TodayForm, { TodayFormData } from "./TodayForm";
 
-export default async function Page ({ params }: { params: { date: string }}) {
+export default async function Page({ params }: { params: { date: string } }) {
   const date = params.date;
 
+  //前回の記録を表示
+  const { data: prevRecord } = await supabase
+    .from("records")
+    .select("*")
+    .lt("date", date)
+    .order("date", { ascending: false })
+    .limit(1);
+
+    const prev = prevRecord?.[0] ?? null;
+
+  //レコード取得
   const { data: record, error } = await supabase
-  .from("records")
-  .select("*")
-  .eq("date", date)
-  .single();
+    .from("records")
+    .select("*")
+    .eq("date", date)
+    .single();
 
   if (error) {
-  console.error(error);
-}
-
+    console.error(error);
+  }
 
   //新規 or 編集の判定
   const isEdit = !!record;
+  const defaultValues: TodayFormData = record
+    ? {
+      date: record.date,
+      weight: record.weight?.toString() ?? null,
+      steps: record.steps?.toString() ?? null,
+      memo: record.memo ??  "",
+    }
+    : {
+      date,
+      weight: null,
+      steps: null,
+      memo: "",
+    };
+  /*************** today/calendarでの差異表示に使用
+  //前回のweightレコード
+  const prevWeight = await supabase
+  .from("records")
+  .select("date, weight")
+  .lt("date", date)
+  .not("weight", "is", null)
+  .order("date", { ascending: false })
+  .limit(1);
 
-  const defaultValues = record ?? {
-    date,
-    memo: "",
-    condition: "",
-  };
-
+  //前回のstepsレコード
+  const prevSteps = await supabase
+  .from("records")
+  .select("date, steps")
+  .lt("date", date)
+  .not("steps", "is", null)
+  .order("date", { ascending: false })
+  .limit(1);
+*/
   return (
     <TodayForm
       defaultValues={defaultValues}
-      isEdit = { isEdit }
-      recordId = { record?.id ?? null }
-      />
+      isEdit={isEdit}
+      recordId={record?.id ?? null}
+      prevRecord={prev}
+    />
   );
 }
