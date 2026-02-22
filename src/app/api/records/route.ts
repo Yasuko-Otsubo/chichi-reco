bbd58a066d2f2cf092efb2efb1b28c9b1a27817f
yasuko <y.otsubo04@gmail.com>
2026-02-22 22:58:19 +0900

    CreateRecordsRequestBody の作成と POST 処理の追加、setMonth などの軽微な修正

--- src/app/api/records/route.ts
+++ src/app/api/records/route.ts
@@ -1,7 +1,20 @@
 import { prisma } from "@/app/_libs/prisma"
-import { NextResponse } from "next/server"
+import { getProfileByUserId } from "@/app/_libs/prisma/profile"
+import { getAuthenticatedUser } from "@/app/_libs/supabase/auth"
+import { NextRequest, NextResponse } from "next/server"
 
-export type PostsIndexResponse = {
+export type CreateRecordRequestBody = {
+  date: string
+  weight: number | null
+  steps: number | null
+  memo: string | null
+}
+
+export type CreateRecordResponse = {
+  id: number
+}
+
+export type RecordsIndexResponse = {
   records: {
     id: number
     date: Date
@@ -14,12 +27,42 @@ export type PostsIndexResponse = {
   }[]
 }
 
-export const GET = async (req: Request) => {
-  const { searchParams } = new URL(req.url)
-  const month = searchParams.get("month")
-  const range = searchParams.get("range")
+export const POST = async (request: NextRequest) => {
+  try {
+    const user = await getAuthenticatedUser(request);
+    const profile = await getProfileByUserId(user.id);
+
+    const body : CreateRecordRequestBody = await request.json()
+    const { date, weight, steps, memo } = body
+
+    const data = await prisma.record.create({
+      data: {
+        profileId: profile.id,
+        date: new Date(date),
+        weight,
+        steps,
+        memo,
+      },
+    })
+      return NextResponse.json<CreateRecordResponse>({
+    id: data.id,
+  })
+} catch (error) {
+  if(error instanceof Error){
+    return NextResponse.json({ message: error.message }, { status: 400 })
+  }
+}
+}
+
+export const GET = async (request: NextRequest) => {
   try {
   //month
+  const user = await getAuthenticatedUser(request);
+  const profile = await getProfileByUserId(user.id);  
+  const { searchParams } = new URL(request.url)
+  const month = searchParams.get("month")
+  const range = searchParams.get("range")
+
   if (month) {
     const year = Number(month.slice(0, 4))
     const m = Number(month.slice(4,6))
@@ -29,6 +72,7 @@ export const GET = async (req: Request) => {
 
     const records = await prisma.record.findMany({
       where: {
+      profileId: profile.id,
         date: {
           gte: from,
           lte: to,
@@ -49,21 +93,22 @@ export const GET = async (req: Request) => {
         from.setDate(today.getDate() - 6)
         break
       case "1month":
-        from.setDate(today.getMonth() - 1)
+        from.setMonth(today.getMonth() - 1)
         break
       case "6month":
-        from.setDate(today.getMonth() - 6)
+        from.setMonth(today.getMonth() - 6)
         break
       case "1Year":
-        from.setDate(today.getFullYear() - 1)
+        from.setFullYear(today.getFullYear() - 1)
         break
       case "3Year":
-        from.setDate(today.getFullYear() - 3)
+        from.setFullYear(today.getFullYear() - 3)
         break
     }
     return NextResponse.json({
       records: await prisma.record.findMany({
         where: {
+          profileId: profile.id,
           date: {
             gte: from,
             lte: today,
@@ -77,6 +122,9 @@ export const GET = async (req: Request) => {
   //全件
   return NextResponse.json({
     records: await prisma.record.findMany({
+      where: {
+        profileId: profile.id,
+      },
       orderBy: { date: "asc" }
     }),
   })
