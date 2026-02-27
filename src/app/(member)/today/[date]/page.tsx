e51c922f6fb8b7bd331ab92bcbc0a5b958e12a51
yasuko <y.otsubo04@gmail.com>
2026-02-27 23:52:13 +0900

    データ表示の実装（idをdateに変更するべき）

--- src/app/(member)/today/[date]/page.tsx
+++ src/app/(member)/today/[date]/page.tsx
@@ -1,75 +1,44 @@
-import { supabase } from "@/app/_libs/supabase";
-import TodayForm, { TodayFormData } from "../_components/TodayForm";
+'use client'
 
-export default async function Page({ params }: { params: { date: string } }) {
-  const date = params.date;
+import { useSupabaseSession } from "@/app/_hooks/useSupabaseSession";
+import { RecordResponse } from "@/app/api/records/[id]/route";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-  //前回の記録を表示
-  const { data: prevRecord } = await supabase
-    .from("records")
-    .select("*")
-    .lt("date", date)
-    .order("date", { ascending: false })
-    .limit(1);
+export default function Page() {
+  // ===== params / auth ===== 
+  const { id } = useParams<{id: string}>();
+  const router = useRouter();
+  const { token } = useSupabaseSession();
 
-    const prev = prevRecord?.[0] ?? null;
+  // ===== 表示系・フォーム用 state =====
+  const [date, setDate] = useState("");
+  const [weight, setWeight] = useState("");
+  const [steps, setSteps] = useState("");
+  const [memo, setMemo] = useState("");
 
-  //レコード取得
-  const { data: record, error } = await supabase
-    .from("records")
-    .select("*")
-    .eq("date", date)
-    .single();
+  // ===== 取得データ =====
+  const [record, setRecord] = useState<RecordResponse['record'] | null >(null);
 
-  if (error) {
-    console.error(error);
-  }
+  // ===== UI制御 =====
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  //新規 or 編集の判定
-  const isEdit = !!record;
-  const defaultValues: TodayFormData = record
-    ? {
-      date: record.date,
-      weight: record.weight?.toString() ?? null,
-      steps: record.steps?.toString() ?? null,
-      memo: record.memo ??  "",
+  // ******* GET *******
+  useEffect(() => {
+    if (!token) return
+
+    const fetcher = async () => {
+      const res = await fetch(`/api/records/${id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token,
+        },
+      })
+      const { record }: RecordResponse = await res.json();
+      setRecord(record);
     }
-    : {
-      date,
-      weight: null,
-      steps: null,
-      memo: "",
-    };
-  /*************** today/calendarでの差異表示に使用
-  //前回のweightレコード
-  const prevWeight = await supabase
-  .from("records")
-  .select("date, weight")
-  .lt("date", date)
-  .not("weight", "is", null)
-  .order("date", { ascending: false })
-  .limit(1);
 
-  //前回のstepsレコード
-  const prevSteps = await supabase
-  .from("records")
-  .select("date, steps")
-  .lt("date", date)
-  .not("steps", "is", null)
-  .order("date", { ascending: false })
-  .limit(1);
-*/
+    fetcher()
+  }, [token, id])
 
-if (!defaultValues) {
-  return null; // or ローディング
-}
-
-  return (
-    <TodayForm
-      defaultValues={defaultValues}
-      isEdit={isEdit}
-      recordId={record?.id ?? null}
-      prevRecord={prev}
-    />
-  );
 }
