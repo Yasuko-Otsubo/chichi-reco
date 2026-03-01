2d8acb44565b336eda98e6612807f8167b053724
yasuko <y.otsubo04@gmail.com>
2026-03-02 01:07:34 +0900

    日本時間で取得

--- src/app/(member)/today/page.tsx
+++ src/app/(member)/today/page.tsx
@@ -1,6 +1,10 @@
 import { redirect } from "next/navigation";
 
 export default function Page() {
-  const today = new Date().toISOString().slice(0, 10);
-  redirect(`/today/${today}`);
+  const today = new Date();
+    // ===== 今日の日付を日本時間で取得 =====
+  const paramDate = `${today.getFullYear()}-${(today.getMonth()+1).toString()
+    .padStart(2,'0')}-${today.getDate().toString().padStart(2, '0')}`;
+
+  redirect(`/today/${paramDate}`);
 }
