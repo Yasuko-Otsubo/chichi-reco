f8773a807139c63e912bdcc405720438835983fd
yasuko <y.otsubo04@gmail.com>
2026-03-02 19:08:01 +0900

    今日の日付を計算し、[date]/page.tsxに飛ばす

--- src/app/(member)/today/page.tsx
+++ src/app/(member)/today/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 
 export default function Page() {
   const today = new Date();
-    // ===== 今日の日付を日本時間で取得 =====
+  // ===== 今日の日付を日本時間で取得 =====
   const paramDate = `${today.getFullYear()}-${(today.getMonth()+1).toString()
     .padStart(2,'0')}-${today.getDate().toString().padStart(2, '0')}`;
 
