3d4c190cebff307154276849ae6c11ce39024a91
yasuko <y.otsubo04@gmail.com>
2026-03-03 18:52:10 +0900

    POST実装

--- src/app/(member)/today/[date]/page.tsx
+++ src/app/(member)/today/[date]/page.tsx
@@ -1,17 +1,18 @@
-'use client'
+"use client";
 
 import { useSupabaseSession } from "@/app/_hooks/useSupabaseSession";
 import { RecordData, RecordResponse } from "@/app/api/records/[date]/route";
+import { CreateRecordRequestBody } from "@/app/api/records/route";
 import { useParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Page() {
-  // ===== auth ===== 
+  // ===== auth =====
   const router = useRouter();
   const { token } = useSupabaseSession();
 
   // ===== URLパラメータから日付を取得 =====
-const { date: paramDate } = useParams<{ date: string }>();
+  const { date: paramDate } = useParams<{ date: string }>();
 
   // ===== 表示系・フォーム用 state =====
   const [date, setDate] = useState("");
@@ -20,7 +21,7 @@ const { date: paramDate } = useParams<{ date: string }>();
   const [memo, setMemo] = useState("");
 
   // ===== 取得データ =====
-  const [record, setRecord] = useState<RecordResponse['record'] | null >(null);
+  const [record, setRecord] = useState<RecordResponse["record"] | null>(null);
 
   // ===== UI制御 =====
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -30,51 +31,121 @@ const { date: paramDate } = useParams<{ date: string }>();
     if (!token) return;
 
     const fetcher = async () => {
-      try{
-      const res = await fetch(`/api/records/${paramDate}`, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: token,
-        },
-      });
-      if (res.status === 404) {
-        //今日のデータがない場合は空
-        const emptyRecord: RecordData = {
-        id: 0,
-        date: paramDate,
-        weight: null,
-        steps: null,
-        memo: null, 
-      };
-      setRecord(emptyRecord);
-      setDate(emptyRecord.date);
-      setWeight("");
-      setSteps("");
-      setMemo("");
-      return;
-    }
+      try {
+        const res = await fetch(`/api/records/${paramDate}`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: token,
+          },
+        });
+        if (res.status === 404) {
+          //今日のデータがない場合は空
+          const emptyRecord: RecordData = {
+            id: 0,
+            date: paramDate,
+            weight: null,
+            steps: null,
+            memo: null,
+          };
+          setRecord(emptyRecord);
+          setDate(emptyRecord.date);
+          setWeight("");
+          setSteps("");
+          setMemo("");
+          return;
+        }
 
-    const { record }: RecordResponse = await res.json();
-    console.log("API record:", record);
-    setRecord(record);
-    setDate(record.date.slice(0, 10));
-    setWeight(record.weight?.toString() ?? "");
-    setSteps(record.steps?.toString() ?? "");
-    setMemo(record.memo ?? "");
-  } catch (error) {
-    console.error(error);
-  }
-};
+        const { record }: RecordResponse = await res.json();
+        console.log("API record:", record);
+        setRecord(record);
+        setDate(record.date.slice(0, 10));
+        setWeight(record.weight?.toString() ?? "");
+        setSteps(record.steps?.toString() ?? "");
+        setMemo(record.memo ?? "");
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
     fetcher();
-  }, [token, paramDate]); 
+  }, [token, paramDate]);
+
+  // ******* POST *******
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!token) return;
+
+    try {
+      setIsSubmitting(true);
+
+      const body: CreateRecordRequestBody = {
+        date: date,
+        weight: weight ? Number(weight) : null,
+        steps: steps ? Number(steps) : null,
+        memo: memo || null }
+
+      const res = await fetch('/api/records', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token
+        },
+        body: JSON.stringify(body),
+      });
+
+      if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error(errorData.message || 'POSTに失敗しました');
+      }
+
+      await res.json();
+
+      router.push('/today/calendar')
+      alert('記録しました')
+    } catch (error) {
+      console.error('記録に失敗しました:', error)
+      alert('き六に失敗しました')
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
 
   return (
-    <div>
-      <p>date={date}</p>
-      <p>weight={weight}</p>
-      <p>steps={steps}</p>
-      <p>memo={memo}</p>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>日付</label>
+        <input className="border-2" type="text" value={date} readOnly />
+      </div>
+      <div>
+        <label>体重</label>
+        <input
+          className="border-2" 
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>歩数</label>
+        <input
+          className="border-2"  
+          value={steps}
+          onChange={(e) => setSteps(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>一言メモ</label>
+        <input
+          className="border-2" 
+          value={memo}
+          onChange={(e) => setMemo(e.target.value)}
+        />
+      </div>
+      <button type="submit" disabled={isSubmitting}>
+        記録する
+      </button>
+
+
+    </form>
   );
 }
