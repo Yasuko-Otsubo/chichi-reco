800e0b2b07ad69d5d6a8e7796bbcd8e775c2bd53
yasuko <y.otsubo04@gmail.com>
2026-03-12 21:51:44 +0900

    型定義修正とUIの実装

--- src/app/(member)/calendar/_components/Calendar.tsx
+++ src/app/(member)/calendar/_components/Calendar.tsx
@@ -1,15 +1,10 @@
+import styles from "@/app/_styles/Calendar.module.css"
+import { RecordData } from "@/app/api/records/[date]/route"
+
 type CalendarData = {
   day: number | null
   record: RecordData | null
   diff: number | null
-}
-
-type RecordData = {
-  id: string
-  date: string
-  weight: number | null
-  steps: number | null
-  memo: string | null
 }
 
 interface Props {
@@ -20,4 +15,60 @@ interface Props {
   todayYear: number
   todayMonth: number
   todayDate: number
+}
+
+export const Calendar: React.FC<Props> = ({
+  calendarData,
+  year,
+  month,
+  changeMonth,
+  todayYear,
+  todayMonth,
+  todayDate
+}) => {
+    const weekDays = ["日", "月", "火", "水", "木", "金", "土"];
+
+  return(
+    <div className={styles.container}>
+      <button onClick={() => changeMonth(-1)}>⇐</button>
+      <span>
+        {year}年 {month}月
+      </span>
+      <button onClick={() => changeMonth(1)}>⇒</button>
+      <div className={styles.calendar}>
+        {weekDays.map((day) => (
+          <div key={day} className={styles.header}>
+            {day}
+          </div>
+        ))}
+
+        {calendarData.map(({ day, record, diff }, i) => {
+          const isToday =
+            year === todayYear && month === todayMonth && day === todayDate;
+
+          return (
+            <div
+              key={i}
+              className={`${styles.cell} ${isToday ? styles.today : ""}`}
+            >
+              {/* 空白マスか確認 */}
+              {day && <div>{day}</div>}
+
+              {/* 記録があるか確認 */}
+              {record && <div>{record.weight}</div>}
+
+              {/* 前回との差 */}
+              {diff !== null && (
+                <div>
+                  {/* 差分計算 */}
+                  {diff > 0 ? "+" : ""}
+                  {diff.toFixed(1)}
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  )
 }
