a463012fbbbb7199f67efaa59f11dcd5a3e1e1f0
yasuko <y.otsubo04@gmail.com>
2026-03-18 18:50:25 +0900

    contactの型定義

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,13 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-import { ContactFields, ContactResponse } from "@/types/contact";
+import { ContactCreateRequest, ContactCreateResponse } from "@/types/contact";
 import { prisma } from "@/app/_libs/prisma";
+import { ApiResponse } from "@/types/api";
 
 export const POST = async (request: NextRequest) => {
   try {
-    const body: ContactFields = await request.json();
+    const body: ContactCreateRequest = await request.json();
     const { name, email, content } = body;
 
-    if (!name || !email || !content ) {
+    if (!name || !email || !content) {
       throw new Error("すべての項目を記入してください");
     }
 
@@ -19,21 +20,19 @@ export const POST = async (request: NextRequest) => {
       data: { name, email, content },
     });
 
-    const response: ContactResponse = {
+    const response: ContactCreateResponse = {
       status: "OK",
       message: "送信しました",
       id: data.id,
     };
 
-    return NextResponse.json(response);
+    return NextResponse.json(response, { status: 200 });
   } catch (error) {
     if (error instanceof Error) {
-      const response: ContactResponse = {
-        status: "NG",
-        message: error.message,
-      };
-      return NextResponse.json(response, { status: 400 });
+      return NextResponse.json<ApiResponse>(
+        { status: "NG", message: error.message },
+        { status: 400 },
+      );
     }
   }
 };
-
