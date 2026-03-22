1e326b9cb84ed0c2a841019ede04750ac0e835b4
yasuko <y.otsubo04@gmail.com>
2026-03-22 14:51:51 +0900

    graphデータ取得

---
+++ src/app/(member)/graph/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useSupabaseSession } from "@/app/_hooks/useSupabaseSession";
+import { RecordData, RecordsResponse } from "@/types/record";
+import { useEffect, useState } from "react";
+
+export default function GraphPage() {
+  const { token } = useSupabaseSession();
+
+  // ===== 取得データ =====
+  const [range, setRange] = useState("7days");
+  const [records, setRecords] = useState<RecordData[]>([]);
+
+  useEffect(() => {
+    if (!token) return;
+
+    const fetcher = async () => {
+      try {
+        const res = await fetch(`/api/records?range=${range}`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: token,
+          },
+        });
+
+        if (!res.ok) {
+          throw new Error("API error");
+        }
+        const data: RecordsResponse = await res.json();
+        setRecords(data.records);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetcher();
+  }, [token, range]);
+  return (
+  <></>
+)
+}
