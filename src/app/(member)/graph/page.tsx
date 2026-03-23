89c2f9b6a2832acbf0a280748949e05532a295c4
yasuko <y.otsubo04@gmail.com>
2026-03-23 21:56:20 +0900

    Rechartsのインストールと折れ線、棒グラフの実装（表示のみ）

--- src/app/(member)/graph/page.tsx
+++ src/app/(member)/graph/page.tsx
@@ -3,6 +3,7 @@
 import { useSupabaseSession } from "@/app/_hooks/useSupabaseSession";
 import { RecordData, RecordsResponse } from "@/types/record";
 import { useEffect, useState } from "react";
+import { Bar, ComposedChart, Line, XAxis, YAxis } from "recharts";
 
 export default function GraphPage() {
   const { token } = useSupabaseSession();
@@ -34,7 +35,55 @@ export default function GraphPage() {
     };
     fetcher();
   }, [token, range]);
+
   return (
-  <></>
-)
+    <>
+      <div className="h-100%">
+        <div className="flex justify-center">
+          <button
+            className="border-2 p-px mx-[3px]"
+            onClick={() => setRange("7days")}
+          >
+            週
+          </button>
+          <button
+            className="border-2 p-px mx-[3px]"
+            onClick={() => setRange("1month")}
+          >
+            月
+          </button>
+          <button
+            className="border-2 p-px mx-[3px]"
+            onClick={() => setRange("6month")}
+          >
+            6か月
+          </button>
+          <button
+            className="border-2 p-px mx-[3px]"
+            onClick={() => setRange("1year")}
+          >
+            年
+          </button>
+          <button
+            className="border-2 p-px mx-[3px]"
+            onClick={() => setRange("3year")}
+          >
+            全期間
+            <br />
+            （最大3年）
+          </button>
+          {/*UXの時に<br>→<span>で処理 */}
+        </div>
+
+        <ComposedChart width={500} height={300} data={records}>
+          <XAxis dataKey="date" stroke="var(--color-text-3)" />
+          <YAxis yAxisId="left" domain={[50, 70]} />
+          <YAxis yAxisId="right" orientation="right" domain={[0, 10000]} />
+          <Line yAxisId="left" dataKey="weight"></Line>
+          <Bar yAxisId="right" dataKey="steps" fill="pink"></Bar>
+          <></>
+        </ComposedChart>
+      </div>
+    </>
+  );
 }
