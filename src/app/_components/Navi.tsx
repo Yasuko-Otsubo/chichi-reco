4dca4ab7c1758a822463be811583092d524bd562
yasuko <y.otsubo04@gmail.com>
2026-03-26 15:31:58 +0900

    ナビゲーションの配置

---
+++ src/app/_components/Navi.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+
+export const Navi: React.FC = () => {
+  return (
+    <div className="flex">
+      <Link href="/today" className="border-2 p-4">
+        入力
+      </Link>
+      <Link href="/calendar" className="border-2 p-4">
+        カレンダー
+      </Link>
+      <Link href="/graph" className="border-2 p-4">
+        グラフ
+      </Link>
+      <Link href="/setting" className="border-2 p-4">
+        設定
+      </Link>
+    </div>
+  );
+};
