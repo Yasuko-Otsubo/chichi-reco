518047ececf67585dba47c65dd66d0e59ab46246
yasuko <y.otsubo04@gmail.com>
2026-04-12 16:17:28 +0900

    リンク先修正と使い方ページ

---
+++ src/app/(member)/settings/support/howto/page.tsx
@@ -0,0 +1,6 @@
+export default function HowtoPage() {
+  return (
+    <div>ここにサポートページ</div>
+
+  )
+}
--- src/app/(member)/settings/support/page.tsx
+++ src/app/(member)/settings/support/page.tsx
@@ -4,16 +4,16 @@ export default function IntroPage() {
   return (
     <main>
       <div className="flex flex-col " >
-        <Link href={"/support/howto"} className="border p-2 m-2">
+        <Link href={"/settings/support/howto"} className="border p-2 m-2">
           使い方ページ
         </Link>
-        <Link href={"/support/faq"} className="border p-2 m-2">
+        <Link href={"/settings/support/faq"} className="border p-2 m-2">
           よくある質問
         </Link>
-        <Link href={"/support/contact"} className="border p-2 m-2">
+        <Link href={"/settings/support/contact"} className="border p-2 m-2">
           お問い合わせ
         </Link>
-        <Link href={"/support/inquiry"} className="border p-2 m-2">
+        <Link href={"/settings/support/inquiry"} className="border p-2 m-2">
           利用規約・プライバシーポリシー
         </Link>
       </div>
