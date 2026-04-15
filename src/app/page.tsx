547b0bacd6eb085d27a8aa96bfbb635fe24628ce
yasuko <y.otsubo04@gmail.com>
2026-04-15 23:50:36 +0900

    トップのcss 修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,37 +3,60 @@ import Link from "next/link";
 
 export default function IntroPage() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center  text-center bg-gradient-to-b from-white to-gray-100 dark:from-gray-900 dark:to-gray-800 ">
-      <header className="flex justify-between w-full">
-        <h1>父レコ</h1>
-        <div className="mb-2">
-          <Link href="/signup" className="p-2 border border border-solid mr-[2px]">新規登録</Link>
-          <Link href="/login" className="p-2 border border border-solid mr-[2px]">ログイン</Link>
-
+    <main className="flex flex-col items-center justify-center  text-center bg-gradient-to-b">
+      <header className="flex justify-between w-full px-2 items-center">
+        <h1 className="text-2xl">父レコ</h1>
+        <div className="flex">
+          <Link
+            href="/signup"
+            className="pl-4 py-2  flex justify-center items-center flex-col"
+          >
+            <Image
+              src="/signup.png"
+              alt="新規登録アイコン"
+              width={30}
+              height={30}
+              className="w-6 h-6 sm:w-8 sm:h-8"
+            />
+            新規登録
+          </Link>
+          <Link
+            href="/login"
+            className="pl-4 flex justify-center items-center mr-[2px] flex-col"
+          >
+            <Image
+              src="/login.png"
+              alt="ログインアイコン"
+              width={30}
+              height={30}
+              className="w-6 h-6 sm:w-8 sm:h-8"
+            />
+            ログイン
+          </Link>
         </div>
       </header>
-
-      <Image
-        src="/top-hero-bg.png"
-        alt="父レコのロゴ"
-        width={600}
-        height={400}
-        className="w-full max-w-[500px] h-auto object-contain bg-[#e6f7ff]"
-      />
-      <h2 className="text-3xl sm:text-5xl font-bold tracking-tight mb-4 text-gray-900 dark:text-white">
-        家族の健康を、やさしく記録。
+      <div className="w-full aspect-[3/2] relative mb-8">
+        <Image
+          src="/top-hero-bg.png"
+          alt="父レコのロゴ"
+          fill
+          className="object-cover"
+        />
+      </div>
+      <h2 className="text-xl sm:text-5xl tracking-tight mb-4 text-gray-900 dark:text-white">
+        毎日の健康を簡単記録
       </h2>
-      <p className="text-lg sm:text-xl text-gray-700 dark:text-gray-300 max-w-xl mb-8">
-        父レコは、家族の健康記録を安心して続けられるアプリです。
-        <br />
+      <p className="text-sm sm:text-xl text-gray-700 dark:text-gray-300 max-w-xl mb-8">
+        体重や歩数を記録するだけで、グラフで見える！<br />
+        今日から始める優しい健康習慣 <br />
         シンプルで美しいUI、記録しやすいUX、そして守られるプライバシー。
       </p>
       <div className="flex flex-col sm:flex-row gap-4">
         <Link
           href="/signup"
-          className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-lg transition"
+          className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-12 rounded-lg transition"
         >
-          はじめてみる
+          今すぐ始める
         </Link>
       </div>
     </main>
