1911559eecc7374baa4783643b918cf521503cd6
yasuko <y.otsubo04@gmail.com>
2026-04-17 22:19:05 +0900

    Naviレイアウト変更

--- src/app/_components/Navi.tsx
+++ src/app/_components/Navi.tsx
@@ -1,21 +1,26 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 export const Navi: React.FC = () => {
   return (
-    <div className="flex">
-      <Link href="/today" className="border-2 p-4">
+    <div className="flex w-full">
+      <Link href="/today" className="flex flex-col items-center justify-center p-4 text-textColor flex-1">
+        <Image src="/pen.png" alt="入力ボタン" width={30} height={30} className="mb-2" />
         入力
       </Link>
-      <Link href="/calendar" className="border-2 p-4">
+      <Link href="/calendar" className="flex flex-col items-center justify-center p-4 text-textColor flex-1">
+        <Image src="/calendar.png" alt="入力ボタン" width={30} height={30} className="mb-2" />
         カレンダー
       </Link>
-      <Link href="/graph" className="border-2 p-4">
+      <Link href="/graph" className="flex flex-col items-center justify-center p-4 text-textColor flex-1">
+        <Image src="/graph.png" alt="入力ボタン" width={30} height={30} className="mb-2" />
         グラフ
       </Link>
-      <Link href="/settings" className="border-2 p-4">
+      <Link href="/settings" className="flex flex-col items-center justify-center p-4 text-textColor flex-1">
+        <Image src="/setting.png" alt="入力ボタン" width={30} height={30} className="mb-2"/>
         設定
       </Link>
     </div>
