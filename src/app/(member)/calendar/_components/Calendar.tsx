3a69e64cbef970a80bfd648a52deea23045fc9cd
yasuko <y.otsubo04@gmail.com>
2026-04-19 19:07:11 +0900

    セル、大枠の修正

--- src/app/(member)/calendar/_components/Calendar.tsx
+++ src/app/(member)/calendar/_components/Calendar.tsx
@@ -30,37 +30,36 @@ export const Calendar: React.FC<Props> = ({
   const weekDays = ["日", "月", "火", "水", "木", "金", "土"];
 
   return (
-    <div className={styles.container}>
-      <button onClick={() => changeMonth(-1)}>⇐</button>
-      <span>
-        {year}年 {month}月
-      </span>
-      <button onClick={() => changeMonth(1)}>⇒</button>
-      <div className={styles.calendar}>
-        {weekDays.map((day) => (
-          <div key={day} className={styles.header}>
-            {day}
-          </div>
-        ))}
+    <>
+      <h1 className="text-xl text-center py-6">今日の記録</h1>
+      <div className={styles.container}>
+        <button onClick={() => changeMonth(-1)}>⇐</button>
+        <span>
+          {year}年 {month}月
+        </span>
+        <button onClick={() => changeMonth(1)}>⇒</button>
+        <div className={styles.calendar}>
+          {weekDays.map((day) => (
+            <div key={day} className={styles.header}>
+              {day}
+            </div>
+          ))}
 
-        {calendarData.map(({ day, record, diff}, i) => {
-          const isToday = 
-          year  === todayYear &&
-          month === todayMonth &&
-          day === todayDate;
-          return (
-            <CalendarCell
-              key={i}
-              day={day}
-              record={record}
-              diff={diff}
-              isToday={isToday}
+          {calendarData.map(({ day, record, diff }, i) => {
+            const isToday =
+              year === todayYear && month === todayMonth && day === todayDate;
+            return (
+              <CalendarCell
+                key={i}
+                day={day}
+                record={record}
+                diff={diff}
+                isToday={isToday}
               />
-          );
-        })}
-
-
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
