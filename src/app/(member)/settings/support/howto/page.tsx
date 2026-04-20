27c3704cdcb9a8ac9f6cccc4c3c87affb84685cf
yasuko <y.otsubo04@gmail.com>
2026-04-21 00:05:41 +0900

    使い方ページUX対応

--- src/app/(member)/settings/support/howto/page.tsx
+++ src/app/(member)/settings/support/howto/page.tsx
@@ -1,6 +1,10 @@
 export default function HowtoPage() {
   return (
-    <div>ここにサポートページ</div>
-
-  )
+    <>
+      <h1 className="text-xl text-center py-6">使い方</h1>
+      <div className=" bg-white rounded-[15px] mb-6 p-2">
+        <p className="text-center"> 作成中 </p>
+      </div>
+    </>
+  );
 }
