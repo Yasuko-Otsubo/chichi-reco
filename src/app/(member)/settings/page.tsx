e5fc6feafad647068c13b2ec53d2fe1f892bc21c
yasuko <y.otsubo04@gmail.com>
2026-04-20 23:10:51 +0900

    設定ページUX

--- src/app/(member)/settings/page.tsx
+++ src/app/(member)/settings/page.tsx
@@ -3,9 +3,14 @@ import Link from "next/link";
 export default function IntroPage() {
   return (
     <main>
-      <div>
-        <Link href={"/settings/mypage"} className="border p-2 m-2">マイページ</Link>
-        <Link href={"/settings/support"} className="border p-2 m-2">サポートページ</Link>
+      <h1 className="text-xl text-center py-6">設定</h1>
+      <div className="flex flex-col bg-white rounded-[15px] py-10 px-6 mb-4" >
+        <Link href={"/settings/mypage"} className="bg-choiceBtn text-center w-[80%] mx-auto p-2 m-2 mb-6 rounded-[10px]">
+          マイページ
+        </Link>
+        <Link href={"/settings/support"} className="bg-choiceBtn text-center w-[80%] mx-auto p-2 m-2 rounded-[10px]">
+          サポートページ
+        </Link>
       </div>
     </main>
   );
