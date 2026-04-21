f81fd07106b513216e0edc5bd7d0c2cc157df1d5
yasuko <y.otsubo04@gmail.com>
2026-04-21 21:33:48 +0900

    ルールの追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,9 +11,10 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-{
+  {
     rules: {
       "@typescript-eslint/no-this-alias": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
     },
   },
 ];
