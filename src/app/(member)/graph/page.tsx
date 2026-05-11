962954c37dcc63509cc595808dcee66c5271502e
yasuko <y.otsubo04@gmail.com>
2026-05-12 00:36:34 +0900

    グラフの画面表示

--- src/app/(member)/graph/page.tsx
+++ src/app/(member)/graph/page.tsx
@@ -8,13 +8,13 @@ export default function GraphPage() {
   const { chartData, range, setRange } = useGraphData();
 
   const tagClass = (r: string) =>
-    `py-1 mx-[3px] w-full text-xs xs:text-sm md:text-base shadow-[0_6px_4px_-2px_rgba(0,0,0,0.2)] transition-all active:translate-y-0.5 active:shadow-none ${range === r ? "bg-tagChoice text-white" : "bg-white"}`;
+    `py-3 mx-[3px] w-full text-xs  shadow-[0_6px_4px_-2px_rgba(0,0,0,0.2)] transition-all active:translate-y-0.5 active:shadow-none ${range === r ? "bg-tagChoice text-white" : "bg-white"}`;
 
   return (
     <>
       <h1 className="text-xl text-center py-4">グラフ</h1>
       <div className="w-full ">
-        <div className="flex justify-between text-base mb-6">
+        <div className="flex justify-between text-base mb-4">
           <button
             className={tagClass("7days")}
             onClick={() => setRange("7days")}
@@ -44,8 +44,6 @@ export default function GraphPage() {
             onClick={() => setRange("3year")}
           >
             全期間
-            <br />
-            <span className="text-xs">（最大3年）</span>
           </button>
           {/*UXの時に<br>→<span>で処理 */}
         </div>
