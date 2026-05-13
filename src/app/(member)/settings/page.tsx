2c1d1717c44f9063979bd2c00555b499da030017
yasuko <y.otsubo04@gmail.com>
2026-05-13 18:20:10 +0900

    ログアウト設置

--- src/app/(member)/settings/page.tsx
+++ src/app/(member)/settings/page.tsx
@@ -1,6 +1,16 @@
+"use client";
+
+import { supabase } from "@/app/_libs/supabase";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function IntroPage() {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    router.replace("/");
+  };
   return (
     <main>
       <h1 className="text-xl text-center py-6">設定</h1>
@@ -11,6 +21,13 @@ export default function IntroPage() {
         <Link href={"/settings/support"} className="bg-choiceBtn text-center w-[80%] mx-auto p-2 m-2 rounded-[10px]">
           サポートページ
         </Link>
+        <button
+          onClick={handleLogout}
+          className="text-center w-[80%] mx-auto p-2 m-2 mt-10 rounded-[10px] text-red-400 border border-red-300 hover:bg-red-400 hover:text-white"
+        >
+          ログアウト
+        </button>
+
       </div>
     </main>
   );
