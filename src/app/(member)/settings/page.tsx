f505417f0385a3794ee2d5df17ff64a72f4e85b4
yasuko <y.otsubo04@gmail.com>
2026-05-17 22:07:41 +0900

    ゲストログインにマイページを見せない

--- src/app/(member)/settings/page.tsx
+++ src/app/(member)/settings/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { useSupabaseSession } from "@/app/_hooks/useSupabaseSession";
 import { supabase } from "@/app/_libs/supabase";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function IntroPage() {
   const router = useRouter();
+  const { session } = useSupabaseSession();
+
+  const isGuest = session?.user.email === process.env.NEXT_PUBLIC_GUEST_EMAIL;
 
   const handleLogout = async () => {
     await supabase.auth.signOut();
@@ -14,11 +18,19 @@ export default function IntroPage() {
   return (
     <main>
       <h1 className="text-xl text-center py-6">設定</h1>
-      <div className="flex flex-col bg-white rounded-[15px] py-10 px-6 mb-4" >
-        <Link href={"/settings/mypage"} className="bg-choiceBtn text-center w-[80%] mx-auto p-2 m-2 mb-6 rounded-[10px]">
-          マイページ
-        </Link>
-        <Link href={"/settings/support"} className="bg-choiceBtn text-center w-[80%] mx-auto p-2 m-2 rounded-[10px]">
+      <div className="flex flex-col bg-white rounded-[15px] py-10 px-6 mb-4">
+        {!isGuest && (
+          <Link
+            href={"/settings/mypage"}
+            className="bg-choiceBtn text-center w-[80%] mx-auto p-2 m-2 mb-6 rounded-[10px]"
+          >
+            マイページ
+          </Link>
+        )}{" "}
+        <Link
+          href={"/settings/support"}
+          className="bg-choiceBtn text-center w-[80%] mx-auto p-2 m-2 rounded-[10px]"
+        >
           サポートページ
         </Link>
         <button
@@ -27,7 +39,6 @@ export default function IntroPage() {
         >
           ログアウト
         </button>
-
       </div>
     </main>
   );
