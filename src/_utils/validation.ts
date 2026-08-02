e4452567c8bb15bbfff4d25af1615a95d98cc1a8
yasuko <y.otsubo04@gmail.com>
2026-08-02 17:49:22 +0900

    バリデーションの型定義追加

--- src/_utils/validation.ts
+++ src/_utils/validation.ts
@@ -5,3 +5,19 @@ export const isValidWeight = (weight: number) => {
 export const isValidSteps = (steps: number) => {
   return steps >= 0 && steps <= 40000;
 }
+
+export const isValidMorningSystolic = (morningSystolic: number) => {
+  return morningSystolic >= 70 && morningSystolic <= 200;
+}
+
+export const isValidMorningDiastolic = (morningDiastolic: number) => {
+  return morningDiastolic >= 70 && morningDiastolic <= 200;
+}
+
+export const isValidEveningSystolic = (eveningSystolic: number) => {
+  return eveningSystolic >= 70 && eveningSystolic <= 200;
+}
+
+export const isValidEveningDiastolic = (eveningDiastolic: number) => {
+  return eveningDiastolic >= 70 && eveningDiastolic <= 200;
+}
